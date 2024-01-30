530e9e1f8fb79f5c33c600e8c0350f726a10a0ae
ghostmaga <magzhan.khorshat@bk.ru>
2024-01-31 00:45:07 +0600

    Task 3 fixed

--- SQL_Analysis_Magzhan_Khorshat_HW.sql
+++ SQL_Analysis_Magzhan_Khorshat_HW.sql
@@ -4,32 +4,34 @@
 SELECT
     p.prod_category AS product_category,
     SUM(s.amount_sold) AS total_sales_amount
-FROM SALES s
-INNER JOIN times t 
-ON s.time_id = t.time_id
-INNER JOIN products p 
+FROM sales s
+LEFT JOIN products p 
 ON s.prod_id = p.prod_id
-WHERE t.calendar_year = 1998
+WHERE EXTRACT (YEAR FROM s.time_id) = 1998
 GROUP BY p.prod_category;
   
+
 -- Calculate the average sales quantity by region for a particular product
 SELECT
-    C.CUST_STATE_PROVINCE AS STATE_PROVINCE,
-    SUM(S.QUANTITY_SOLD) AS TOTAL_SALES_QUANTITY,
-    AVG(S.QUANTITY_SOLD) AS AVERAGE_SALES_QUANTITY
-FROM SALES S
-INNER JOIN CUSTOMERS C 
-ON S.CUST_ID = C.CUST_ID
-INNER JOIN PRODUCTS P 
-ON S.PROD_ID = P.PROD_ID
-WHERE P.PROD_ID = 13
-GROUP BY
-    C.CUST_STATE_PROVINCE;
-  
+    c2.country_region AS region,
+    SUM(s.quantity_sold) AS total_sales_quantity,
+    AVG(s.quantity_sold) AS average_sales_quantity
+FROM sales s
+INNER JOIN customers c 
+ON s.cust_id = c.cust_id
+INNER JOIN countries c2 
+ON c2.country_id = c.country_id 
+INNER JOIN products p 
+ON s.prod_id = p.prod_id
+WHERE p.prod_id = (SELECT prod_id FROM products WHERE upper(prod_name) = '5MP TELEPHOTO DIGITAL CAMERA')
+GROUP BY c2.country_region
+ORDER BY SUM(s.quantity_sold) DESC;
+    
+
 -- Find the top five customers with the highest total sales amount
 SELECT
     c.cust_id AS customer_id,
-    c.cust_first_name || ' ' || c.cust_last_name AS customer_name,
+    COALESCE(upper(c.cust_first_name) || ' ' || upper(c.cust_last_name), '') AS customer_name,
     SUM(s.amount_sold) AS total_sales_amount
 FROM SALES s
 INNER JOIN customers c 
