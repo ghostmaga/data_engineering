d09a556bd25f3c050c36876386aa7cd8993eb13f
ghostmaga <magzhan.khorshat@bk.ru>
2024-02-01 17:15:30 +0600

    SQL_DCL_HW updated

--- SQL_DCL_Magzhan_Khorshat_HW_Tasks_2_3.sql
+++ SQL_DCL_Magzhan_Khorshat_HW_Tasks_2_3.sql
@@ -3,9 +3,11 @@ SET search_path TO public;
 
 DROP POLICY IF EXISTS customer_policy_rental ON rental;
 DROP POLICY IF EXISTS customer_policy_payment ON payment;
+--DROP POLICY IF EXISTS customer_policy ON customer;
 
 ALTER TABLE rental DISABLE ROW LEVEL SECURITY;
 ALTER TABLE payment DISABLE ROW LEVEL SECURITY;
+--ALTER TABLE customer DISABLE ROW LEVEL SECURITY;
 
 -- TASK 2 --
 
@@ -73,7 +75,10 @@ BEGIN
 		INSERT INTO rental (rental_date, inventory_id, customer_id, staff_id)
 		SELECT * FROM 	(
 			VALUES 
-				('2005-05-26 01:54:33.000 +0600'::timestamptz, 1, 1, 1)
+				('2005-05-26 01:54:33.000 +0600'::timestamptz, 
+				(SELECT i.inventory_id FROM inventory i INNER JOIN film f ON f.film_id = (SELECT film_id FROM film WHERE upper(title) = 'ACADEMY DINOSAUR') LIMIT 1), 
+				(SELECT customer_id FROM customer WHERE (COALESCE (upper(first_name) || ' ' || upper(last_name), '') = 'MARY SMITH')), 
+				(SELECT store_id FROM store s INNER JOIN address a ON a.address_id = s.address_id WHERE upper(a.address) = '47 MYSAKILA DRIVE' LIMIT 1))
 		) AS rnt(rental_date, iid, cid, sid)
 		WHERE NOT EXISTS (
 			SELECT 1 
@@ -108,6 +113,7 @@ END $$;
 
 -- 6 --
 SET ROLE postgres;
+
 -- Create a personalized role for any customer already existing in the dvd_rental database. 
 -- The name of the role name must be client_{first_name}_{last_name} (omit curly brackets). 
 -- The customer's payment and rental history must not be empty. 
@@ -147,7 +153,7 @@ BEGIN
 	      EXECUTE 'CREATE ROLE ' || role_name;
 	      EXECUTE 'GRANT CONNECT ON DATABASE dvdrental TO ' || role_name;
 	      EXECUTE 'GRANT USAGE ON SCHEMA public TO ' || role_name;
---	      EXECUTE 'GRANT SELECT ON TABLE customer TO ' || role_name;  
+	      EXECUTE 'GRANT SELECT ON TABLE customer TO ' || role_name;  
 	      RAISE NOTICE 'Personalized role % has been created.', role_name;
 	   ELSE
 	   	RAISE NOTICE 'Role % already exists.', role_name;
@@ -166,12 +172,39 @@ ALTER TABLE rental ENABLE ROW LEVEL SECURITY; -- by default it is disabled
 ALTER TABLE payment ENABLE ROW LEVEL SECURITY;
 
 --FOR testing purposes
-GRANT SELECT ON TABLE customer TO client_mary_smith;	
-GRANT SELECT, UPDATE, INSERT ON TABLE rental TO client_mary_smith; 
+GRANT SELECT ON TABLE rental TO client_mary_smith; 
 GRANT SELECT ON TABLE payment TO client_mary_smith; 
+GRANT SELECT ON TABLE customer TO client_mary_smith;
 
--- Restrict access to the 'customer' table
-REVOKE SELECT ON TABLE customer FROM public;
+-- Create a function to get customer ID
+--CREATE OR REPLACE FUNCTION get_current_customer_id(fname TEXT, lname TEXT)
+--RETURNS INTEGER AS $$
+--BEGIN
+--    RETURN (
+--        SELECT customer_id
+--        FROM customer c
+--        WHERE lower(c.first_name) = lower(fname)
+--        AND lower(c.last_name) = lower(lname)
+--        LIMIT 1
+--    );
+--END;
+--$$ LANGUAGE plpgsql;
+
+--SET SESSION AUTHORIZATION client_mary_smith;
+
+--CREATE POLICY customer_policy
+--	ON customer
+--   USING (customer_id = get_current_customer_id((substring(current_user from 'client_(\w+)_'))::text, (substring(current_user from '_([^_]+)$'))::text));
+
+-- Create a RLS policy on the 'rental' table
+--CREATE POLICY customer_policy
+--	ON customer
+--   USING (customer_id = (
+--   	SELECT customer_id 
+--    	FROM customer 
+--    	WHERE lower(first_name) = substring(current_user from 'client_(.*?)_') 
+--    	AND lower(last_name) = substring(current_user from '_([^_]+)$')
+--	));
 
 -- Create a RLS policy on the 'rental' table
 CREATE POLICY customer_policy_rental
@@ -179,8 +212,8 @@ CREATE POLICY customer_policy_rental
    USING (customer_id = (
    	SELECT customer_id 
     	FROM customer 
-    	WHERE first_name = substring((SELECT rolname FROM pg_roles WHERE rolname = 'client_mary_smith') from 'client_(.*?)_') 
-    	AND last_name = substring((SELECT rolname FROM pg_roles WHERE rolname = 'client_mary_smith') from '_([^_]+)$')
+    	WHERE lower(first_name) = substring(current_user from 'client_(.*?)_') 
+    	AND lower(last_name) = substring(current_user from '_([^_]+)$')
 	));
 
 -- Create a RLS policy on the 'payment' table
@@ -188,10 +221,16 @@ CREATE POLICY customer_policy_payment
 	ON payment
    USING (customer_id = (
    	SELECT customer_id 
-    	FROM customer 
-    	WHERE (first_name) = upper(substring((SELECT rolname FROM pg_roles WHERE rolname = 'client_mary_smith') from 'client_(.*?)_')) 
-    	AND upper(last_name) = upper(substring((SELECT rolname FROM pg_roles WHERE rolname = 'client_mary_smith') from '_([^_]+)$'))
+    	FROM customer  
+    	WHERE lower(first_name) = substring(current_user from 'client_(.*?)_') 
+    	AND lower(last_name) = substring(current_user from '_([^_]+)$')
 	));
 
+--ALTER TABLE customer ENABLE ROW LEVEL SECURITY; -- enabling here to avoid infinite recursion
+--RESET SESSION AUTHORIZATION;
+
 SET ROLE client_mary_smith;
+
+--select * from customer;
 SELECT * FROM rental;
+SELECT * FROM payment;
